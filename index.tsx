9d9f048a200a28a838dbaa57f539404fdd45e821
Lucas Huerta <huertalucas13@gmail.com>
2023-10-16 09:59:32 +0200

    [ADD] start bun js

--- index.tsx
+++ index.tsx
@@ -1,8 +1,18 @@
-const server = Bun.serve({
-    port: 3000,
-    fetch(request) {
-      return new Response("Welcome to Bun!");
-    },
-  });
-  
-  console.log(`Listening on localhost:${server.port}`);
+import { serve } from "bun";
+serve({
+  port: 4003,
+
+  fetch(req) {
+    const url = new URL(req.url);
+    if (url.pathname === "/") return new Response("Home page!");
+    if (url.pathname === "/blog") return new Response("Blog page!");
+    return new Response("404 wrong page!");
+  },
+  error(error) {
+    return new Response(`<pre>${error}\n${error.stack}</pre>`, {
+      headers: {
+        "Content-Type": "text/html",
+      },
+    });
+  },
+});
