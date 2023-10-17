f97e41a1d289a1ad0eb6f2b19320fe37c6f61b6b
Lucas Huerta <huertalucas13@gmail.com>
2023-10-17 10:28:15 +0200

    [ADD] Swagger => '/v1/swagger'

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,24 @@ import './database/db.setup';
 import { usersController } from './controllers/user.controller';
 import { pokemonController } from './controllers/pokemon.controller';
 import { authController } from './controllers/auth.controller';
-import { logger } from '@grotto/logysia';
 import { cors } from '@elysiajs/cors';
+import { swagger } from '@elysiajs/swagger';
 
 const PORT = process.env.PORT || 3000;
 export const app = new Elysia();
 
 app
-  // .use(logger())
+  .use(
+      swagger({
+        path: '/v1/swagger',
+        documentation: {
+          info: {
+            title: 'Bun.js CRUD app with Elysia.js',
+            version: '1.0.0',
+          },
+        },
+      })
+  )
   .get('/', () => 'Hello Bun.js!')
   .group('', (app: Elysia) =>
     app
